710ebf662f4d0bf8fb273591a70f93381f9c0b1d
quyet <phamquyet9688@gmail.com>
2023-02-21 09:24:13 +0700

    error

--- dist/index.js
+++ dist/index.js
@@ -7,6 +7,7 @@ const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
 const morgan_1 = __importDefault(require("morgan"));
 const helmet_1 = __importDefault(require("helmet"));
+const fs_1 = __importDefault(require("fs"));
 const PORT = 3000;
 const app = (0, express_1.default)();
 app.use((0, helmet_1.default)());
@@ -16,6 +17,20 @@ app.get('/', (req, res) => {
     res.json({
         message: "Hello!How are you?"
     });
+});
+app.get("/one", (req, res, next) => {
+    fs_1.default.promises.readFile('./one.txt')
+        .then(data => res.send(data))
+        .catch(err => next(err));
+});
+app.use((err, req, res, next) => {
+    console.error('Error 1111: ', err.type);
+    if (err.type == 'time-out') {
+        res.status(408).send(err);
+    }
+    else {
+        res.status(500).send(err);
+    }
 });
 app.listen(PORT, () => {
     console.log("App running on port: " + PORT);
--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import morgan from "morgan";
 import helmet from "helmet";
+import fs from "fs";
 
 const PORT=3000;
 const app=express();
@@ -14,6 +15,19 @@ app.get('/',(req,res)=>{
         message:"Hello!How are you?"
     })
 })
+
+app.get("/one",(req,res,next)=>{
+    fs.promises.readFile('./one.txt')
+        .then(data=>res.send(data))
+        .catch(err=>next(err))
+})
+
+app.use((err,req,res,next)=>{
+    console.error('Error 1111: ', err.type)
+    if(err.type=='time-out'){res.status(408).send(err)}
+    else{res.status(500).send(err)}
+})
+
 app.listen(PORT,()=>{
     console.log("App running on port: " + PORT)
 
