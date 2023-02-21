a6eb4f999bd05988e5e6f455a4a52d4676146347
quyet <phamquyet9688@gmail.com>
2023-02-21 10:22:37 +0700

    routes

--- dist/index.js
+++ dist/index.js
@@ -8,11 +8,15 @@ const body_parser_1 = __importDefault(require("body-parser"));
 const morgan_1 = __importDefault(require("morgan"));
 const helmet_1 = __importDefault(require("helmet"));
 const fs_1 = __importDefault(require("fs"));
+const axios_1 = __importDefault(require("axios"));
+const responseTime_1 = require("./src/middleware/responseTime");
+const router_1 = __importDefault(require("./router/router"));
 const PORT = 3000;
 const app = (0, express_1.default)();
 app.use((0, helmet_1.default)());
 app.use(body_parser_1.default.json());
 app.use((0, morgan_1.default)("common"));
+app.use(responseTime_1.responseTime);
 app.get('/', (req, res) => {
     res.json({
         message: "Hello!How are you?"
@@ -32,6 +36,23 @@ app.use((err, req, res, next) => {
         res.status(500).send(err);
     }
 });
+app.get('/home', async (req, res) => {
+    try {
+        const url = 'https://pokeapi.co/api/v2/ability/?limit=100&offset=0';
+        const response = await axios_1.default.get(url);
+        const data = response.data;
+        if (data) {
+            res.status(200).json(data);
+        }
+        else {
+            res.end('<h1>Error</h1>');
+        }
+    }
+    catch (err) {
+        res.end('<h1>Error</h1>');
+    }
+});
+app.use(router_1.default);
 app.listen(PORT, () => {
     console.log("App running on port: " + PORT);
 });
--- index.ts
+++ index.ts
@@ -3,6 +3,9 @@ import bodyParser from "body-parser";
 import morgan from "morgan";
 import helmet from "helmet";
 import fs from "fs";
+import axios from "axios";
+import {responseTime} from "./src/middleware/responseTime";
+import routes from "./router/router";
 
 const PORT=3000;
 const app=express();
@@ -10,6 +13,7 @@ const app=express();
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(morgan("common"));
+app.use(responseTime);
 app.get('/',(req,res)=>{
     res.json({
         message:"Hello!How are you?"
@@ -27,6 +31,25 @@ app.use((err,req,res,next)=>{
     if(err.type=='time-out'){res.status(408).send(err)}
     else{res.status(500).send(err)}
 })
+
+app.get('/home',async (req,res)=>{
+    try{
+        const url = 'https://pokeapi.co/api/v2/ability/?limit=100&offset=0';
+        const response=await axios.get(url);
+        const data=response.data;
+
+        if(data){
+            res.status(200).json(data);
+        }else{
+            res.end('<h1>Error</h1>')
+        }
+
+    }catch (err){
+        res.end('<h1>Error</h1>')
+    }
+})
+
+app.use(routes)
 
 app.listen(PORT,()=>{
     console.log("App running on port: " + PORT)
